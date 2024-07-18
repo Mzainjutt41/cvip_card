09a77263dae9c24b9bd4846a953b461f18ddf018
Muhammad Zain Aslam <162907950+Mzainjutt41@users.noreply.github.com>
2024-07-18 23:13:32 +0500

    Initial commit

---
+++ card/card.js
@@ -0,0 +1,3 @@
+function showMoreInfo() {
+    alert('Lucifer is a TV series about the Devil who abandons Hell for Los Angeles where he runs his own nightclub and becomes a consultant to the LAPD.');
+}
